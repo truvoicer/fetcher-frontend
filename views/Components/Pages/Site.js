d4c1592ea5dd9ca59f0b21404f43b0b33ccd11f1
truvoice <miky@local.com>
2020-08-06 04:51:32 +0100

    Changeing template

--- views/Components/Pages/Site.js
+++ views/Components/Pages/Site.js
@@ -1,6 +1,4 @@
 import React from "react";
-import {wpApiConfig} from "../../../config/wp-api-config";
-import Header from "../Sidebars/Header";
 import ReactHtmlParser from "react-html-parser";
 import {filterHtml} from "../../../library/html-parser";
 import {ListingsContext} from "../../Context/ListingsContext";
@@ -9,35 +7,25 @@ import Head from "next/head";
 import {connect} from "react-redux";
 import {getPageData} from "../../../redux/middleware/page-middleware";
 
-class PageComponent extends React.Component {
+class SiteComponent extends React.Component {
     constructor(props) {
         super(props);
         this.htmlParserOptions = {
             decodeEntities: true,
             transform: filterHtml
         };
-        this.getPage = this.getPage.bind(this);
-    }
-
-    componentDidMount() {
-        this.getPage();
-    }
-
-    getPage() {
-        this.props.getPageData(buildWpApiUrl(wpApiConfig.endpoints.page, this.props.data.pageName));
     }
 
     render() {
         return (
-            <div id={"page-content"}>
+            <>
                 {this.props.pageData
                     ?
                     <>
                         <Head>
-                            <title>{this.props.pageData.seo_title? this.props.pageData.seo_title : "Loading.."}</title>
-                            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+                            <title>{this.props.pageData.seo_title ? this.props.pageData.seo_title : "Loading.."}</title>
+                            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
                         </Head>
-                    <Header data={this.props.pageData}/>
 
                         <h1>{this.props.pageData.post_title}</h1>
                         {ReactHtmlParser(this.props.pageData.post_content, this.htmlParserOptions)}
@@ -45,20 +33,20 @@ class PageComponent extends React.Component {
                     :
                     <div>Loading...</div>
                 }
-            </div>
+            </>
         )
     }
 }
-PageComponent.contextType = ListingsContext;
+
 function mapStateToProps(state) {
     // console.log(state)
     return {
+        pageName: state.page.pageName,
         pageData: state.page.post
     };
 }
-const Page = connect(
+
+export default connect(
     mapStateToProps,
     {getPageData}
-)(PageComponent);
-
-export default Page;
+)(SiteComponent);
